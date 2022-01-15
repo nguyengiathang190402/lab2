6ad8eb722ac4f4f1bfa84a095f784748c3332308
nguyengiathang190402 <94787151+nguyengiathang190402@users.noreply.github.com>
2022-01-15 14:07:19 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,12 @@
+const data = [
+    {
+        id: "1", title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQde6jJlVw6nZRvCIUKV6ilH7n0sOEaemSzOA&usqp=CAU", desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh Ong vàng Poly, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt",
+    },
+    {
+        id: "2", title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQde6jJlVw6nZRvCIUKV6ilH7n0sOEaemSzOA&usqp=CAU", desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh Ong vàng Poly, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt",
+    },
+    {
+        id: "3", title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQde6jJlVw6nZRvCIUKV6ilH7n0sOEaemSzOA&usqp=CAU", desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh Ong vàng Poly, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt",
+    },
+];
+export default data;
